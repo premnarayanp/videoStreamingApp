33a1f6efb189578b99b132605677ff068f7e57d4
premnarayanp <premnarayanpatel223@gmail.com>
2024-09-02 15:48:08 +0530

    join room, join call ,send  and send offer,get answer , and  enable local video stream over socket

--- client/src/pages/RoomPage.js
+++ client/src/pages/RoomPage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
 // import Peer from 'simple-peer';
 import { useSelector } from 'react-redux';
+import ReactPlayer from "react-player"
+import peer from "../services/peer";
 
 const RoomsPage = () => {
-  // const [remoteSocketId, setRemoteSocketId] = useState(null);
-  // const [myStream, setMyStream] = useState();
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState();
 
 
   // const dispatch = useDispatch();
@@ -14,33 +16,82 @@ const RoomsPage = () => {
 
   const handleUserJoin = useCallback((data) => {
     console.log("user joined From Backend", data);
-    //const { room, email } = data;
+    const { id, email } = data;
+    setRemoteSocketId(id);
 
   }, []);
 
-  // const handleCallUser = useCallback((data) => {
-  //   const stream = navigator.mediaDevices.getUserMedia({
-  //     audio: true,
-  //     video: true
-  //   });
+  const handleCallUser = useCallback(async (data) => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true
+    });
 
-  //   setMyStream(stream);
-  // }, []);
+    const offer = await peer.getOffer();
+    //console.log("offer=====", offer);
+    socket.emit("room-user:call", { to: remoteSocketId, offer: offer });
+    setMyStream(stream);
+  }, [remoteSocketId, socket]);
 
 
+  const handleIncomingCall = useCallback(async (data) => {
+    const { from, offer } = data;
+    console.log("handleIncomingCall", "---from=", from, "---offer=", offer);
+    setRemoteSocketId(from);
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true
+    });
+
+    setMyStream(stream);
+    const ans = await peer.getAnswer(offer);
+    console.log("ans=====", ans);
+    socket.emit("room-user-call:excepted", { to: from, ans: ans });
+
+  }, [socket]);
+
+
+  const handleCallExcepted = useCallback(async (data) => {
+    const { from, ans } = data;
+    console.log("handleCallExcepted", "---from=", from, "---ans=", ans);
+    peer.setLocalDescription(ans);
+  }, []);
 
   useEffect(() => {
     if (socket) {
       socket.on('user:joined', handleUserJoin);
+      socket.on('room:incoming:call', handleIncomingCall);
+      socket.on('room-user-call:excepted', handleCallExcepted);
+
+
       return () => {
         socket.off('user:joined', handleUserJoin);
+        socket.off('room:incoming:call', handleIncomingCall);
+        socket.off('room-user-call:excepted', handleCallExcepted);
       }
     }
 
-  }, [socket, handleUserJoin]);
+  }, [socket, handleUserJoin, handleIncomingCall]);
 
   return (
-    <div>Room Page</div>
+    <div className='RoomPage'>
+      <h1>Room Page</h1>
+      <h4>{remoteSocketId ? "User Joined" : "Nothing anyone in Room"}</h4>
+      {
+        remoteSocketId && <button onClick={handleCallUser}>Call</button>
+      }
+
+      {
+        myStream &&
+        <ReactPlayer
+          playing
+          muted
+          height="200px"
+          width="200px"
+          url={myStream} />
+      }
+
+    </div>
   )
 
 }
--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const emailToSocketIdMap = new Map();
 const socketIdToEmailMap = new Map();
 
 io.on('connection', (socket) => {
-    //console.log('New user connected: ', socket.id);
+    console.log('New user connected: ', socket.id);
 
 
     //======================Single one to one call sockets===========================================
@@ -35,12 +35,12 @@ io.on('connection', (socket) => {
     socket.on("add-user", (user) => {
         emailToSocketIdMap.set(user.email, socket.id);
         socketIdToEmailMap.set(socket.id, user.email);
-        console.log('add-user: ', user);
+        //console.log('add-user: ', user);
     });
 
     // any user doing single call  req for any particular single friend over socket
     socket.on('call:join', (data) => {
-        console.log(data);
+        //console.log(data);
         const { to, from } = data;
         const friendSocketId = emailToSocketIdMap.get(to.email);
         //send  join call for that friend
@@ -58,15 +58,29 @@ io.on('connection', (socket) => {
     //======================Group/Room  call sockets==============================
     //Join room By host
     socket.on('room:join', (data) => {
-        console.log(data);
+        //console.log(data);
         const { room, email } = data;
-        io.to(room.id).emit("user:joined", data);
+        io.to(room.id).emit("user:joined", { email, id: socket.id });
         socket.join(room.id);
         io.to(socket.id).emit("room:join", data);
     });
 
+
+    socket.on('room-user:call', (data) => {
+        console.log("room-user:call-", data);
+        const { to, offer } = data;
+        io.to(to).emit("room:incoming:call", { from: socket.id, offer: offer });
+    });
+
+    socket.on('room-user-call:excepted', (data) => {
+        console.log(" room-user-call:excepted==", data);
+        const { to, ans } = data;
+        io.to(to).emit("room-user-call:excepted", { from: socket.id, ans });
+    });
+
+
     socket.on('user:join', (data) => {
-        console.log(data);
+        console.log("=============user:join=============", data);
         const { room, email } = data;
         io.to(socket.id).emit("room:join", data);
     });
